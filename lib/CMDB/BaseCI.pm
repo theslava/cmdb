61520800a3c060c9df5d583f5f428aba7568cb94
Slava Goltser <slavikg@gmail.com>
2015-12-17 00:43:46 -0500

    make use of MooseX::Types::UUID

--- lib/CMDB/BaseCI.pm
+++ lib/CMDB/BaseCI.pm
@@ -2,10 +2,12 @@ package CMDB::BaseCI;
 
 use Moose;
 use MooseX::Storage;
+use MooseX::Types::UUID qw(UUID);
 use UUID::Tiny ':std';
 
 has 'uuid' => (
     is => 'ro',
+    isa => UUID,
     required => 1,
     default => sub {
         create_uuid_as_string(UUID_V4);
